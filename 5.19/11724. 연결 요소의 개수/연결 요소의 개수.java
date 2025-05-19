5aebbf17544a11c296b3c629bc16f5c7545edf05
EOTAEGYU <wkdrnxorb@naver.com>
2025-05-19 23:22:30 +0900

    백준 11724 연결 요소의 개수

---
+++ 5.19/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
+    static int n, m;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 입력
+        n = sc.nextInt();
+        m = sc.nextInt();
+
+        graph = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+
+        // 그래프 초기화
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 간선 입력
+        for (int i = 0; i < m; i++) {
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int count = 0;
+
+        // 각 정점에 대해 DFS 수행
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    static void dfs(int node) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
